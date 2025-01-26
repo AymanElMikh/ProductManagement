e633cc6d0f5109420daa0a8082a93bf12c999cf9
AymanElMikh <aymanmikh7@gmail.com>
2025-01-26 16:34:16 +0100

    Enhacing class design, make product object immutable

--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -39,10 +39,10 @@ public class Product {
 
     public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
 
-    private int id;
-    private String name;
-    private BigDecimal price;
-    private Rating rating;
+    private final int id;
+    private final String name;
+    private final BigDecimal price;
+    private final Rating rating;
 
     public Product(int id, String name, BigDecimal price, Rating rating) {
         this.id = id;
@@ -63,34 +63,12 @@ public class Product {
         return id;
     }
 
-    public void setId(final int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                ", rating =" + rating.getStars() +
-                '}';
     }
 
     /**
@@ -103,6 +81,7 @@ public class Product {
      *
      * @return the discount amount as a {@code BigDecimal}, rounded to two decimal places
      */
+
     public BigDecimal getDiscount(){
         return  price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
     }
@@ -110,4 +89,21 @@ public class Product {
     public Rating getRating(){
         return this.rating;
     }
+
+    public Product applyRating(final Rating newRating){
+        return new Product(id, name, price, newRating);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", rating =" + rating.getStars() +
+                '}';
+    }
+
+
+
 }
